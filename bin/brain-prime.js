6d64230770862a7da30cd1922430267c9071e802
xocoee <141821998+xocoee@users.noreply.github.com>
2023-09-19 14:52:40 +0300

    add 9 item 2.0

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -5,6 +5,7 @@ const userName = readlineSync.question('May I have your name? ');
 console.log('Hi ' + userName + '!');
 
 const playGame = () => {
+    console.log('Answer "yes" if given number is prime. Otherwise answer "no"');
 
     const primeNumber = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
 
