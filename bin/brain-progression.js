a58ae519773ebdaed0feb5233fb2049ace442bbe
xocoee <141821998+xocoee@users.noreply.github.com>
2023-09-21 17:45:30 +0300

    edit brain-progression

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -12,24 +12,27 @@ const playGameProgression = () => {
   while (correctAnswersCount < 3) {
     const randomNumber = Math.floor(Math.random() * 80) + 1;
     const resultNumber = [];
-    const addTwo = (number) => {
+    const addTwo = (num) => {
+      let numAdd = num;
       for (let i = 0; i < 8; i += 1) {
-        resultNumber.push(number);
-        number += 2;
+        resultNumber.push(numAdd);
+        numAdd += 2;
       }
       return resultNumber;
     };
     addTwo(randomNumber);
     const randomIndex = Math.floor(Math.random() * resultNumber.length);
     const randomNumberIndex = resultNumber[randomIndex];
-    const playerNumbers = resultNumber;
-    const removed = playerNumbers.splice(randomIndex, 1, '..');
-    const phrase = playerNumbers.join(' ');
+    const userResultNumber = resultNumber.slice(0);
+    const showPlayerNumbers = userResultNumber.splice(randomIndex, 1, '..');
+    const strToShowPlayerNumbers = showPlayerNumbers.join();
+    const phrase = userResultNumber.join(' ');
+    const riddledNumber = Number(strToShowPlayerNumbers);
     console.log(`Question: ${phrase}`);
     const userAnswer = readlineSync.question('Your answer: ');
     const userAnswerNumber = Number(userAnswer);
 
-    if (userAnswerNumber === randomNumberIndex) {
+    if (userAnswerNumber === riddledNumber) {
       console.log('Correct!');
       correctAnswersCount += 1;
     } else {
