a4b831849e64d3f72213094c6ab191b3e7427dcf
xocoee <141821998+xocoee@users.noreply.github.com>
2023-09-21 16:41:35 +0300

    edit brain-prime

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -9,33 +9,47 @@ console.log(`Hello, ${userName}!`);
 const playGamePrime = () => {
   console.log('Answer "yes" if given number is prime. Otherwise answer "no"');
 
-  const primeNumber = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
-
   let correctAnswersCount = 0;
   while (correctAnswersCount < 3) {
     const randomNumber = Math.floor(Math.random() * 100) + 1;
+    const primeNumber = (num) => {
+      if (num < 2) {
+        return false;
+      }
+
+      for (let i = 2; i <= num / 2; i += 1) {
+        if (num % 1 === 0) {
+          return false;
+        }
+      }
+      return true;
+    }
+
     console.log(`Question: ${randomNumber}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    const isPresent = primeNumber.includes(randomNumber);
+    //const isPresent = primeNumber.includes(randomNumber);
 
-    if (userAnswer === 'yes' && isPresent) {
-      console.log('Correct!');
-      correctAnswersCount += 1;
-    } else if (userAnswer === 'no' && !isPresent) {
-      console.log('Correct!');
-      correctAnswersCount += 1;
-    } else if (userAnswer === 'yes' && !isPresent) {
-      console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`);
-      return;
-    } else if (userAnswer === 'no' && isPresent) {
-      console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${userName}!`);
-      return;
-    } else {
-      console.log("Input error! 'yes' or 'no'");
-      return;
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
+    console.log(randomNumber);
+    console.log(primeNumber(randomNumber));
+
+  //   if (userAnswer === 'yes' && isPresent) {
+  //     console.log('Correct!');
+  //     correctAnswersCount += 1;
+  //   } else if (userAnswer === 'no' && !isPresent) {
+  //     console.log('Correct!');
+  //     correctAnswersCount += 1;
+  //   } else if (userAnswer === 'yes' && !isPresent) {
+  //     console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${userName}!`);
+  //     return;
+  //   } else if (userAnswer === 'no' && isPresent) {
+  //     console.log(`'no' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, ${userName}!`);
+  //     return;
+  //   } else {
+  //     console.log("Input error! 'yes' or 'no'");
+  //     return;
+  //   }
+  // }
+  // console.log(`Congratulations, ${userName}!`);
 };
 
 playGamePrime();
