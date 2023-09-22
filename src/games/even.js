488a2ffbb6325ee4e6b06c0d6e1eed56a4cc7db0
xocoee <141821998+xocoee@users.noreply.github.com>
2023-09-22 13:32:46 +0300

    edit games

--- src/games/even.js
+++ src/games/even.js
@@ -3,13 +3,13 @@ import run from '../index.js';
 
 const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = (num) => num % 2 === 0;
+const isEven = (number) => number % 2 === 0;
 
 const getQuestionEven = () => {
-  const num = getRandom(1, 100);
-  const question = String(num);
-  const correctUserAnswer = isEven(num) ? 'yes' : 'no';
-  return { question, correctUserAnswer };
+  const number = getRandom(0, 100);
+  const question = String(number);
+  const correctAnswer = isEven(number) ? 'yes' : 'no';
+  return { question, correctAnswer };
 };
 
 const runGameEven = () => run(rules, getQuestionEven);
