b5e4b9bf4568510f63859e976aa2cb31d073a78e
xocoee <141821998+xocoee@users.noreply.github.com>
2023-09-26 13:14:37 +0300

    add brain-games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+import readlineSync from 'readline-sync';
 import salute from '../src/cli.js';
 
 salute();
+const name = readlineSync.question('May I have your name? ');
+console.info(`Hello, ${name}!`);
