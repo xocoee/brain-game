bd671d5f1da37754c654f8172b062455ad35a7bc
xocoee <141821998+xocoee@users.noreply.github.com>
2023-09-28 14:15:49 +0300

    fix

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,11 @@
-const getRandomNumber = (min, max) => {
-  const minNumber = Math.ceil(min);
-  const maxNumber = Math.floor(max);
-  return Math.floor(Math.random() * (maxNumber - minNumber)) + minNumber;
-};
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 export default getRandomNumber;
+
+// const getRandomNumber = (min, max) => {
+//   const minNumber = Math.ceil(min);
+//   const maxNumber = Math.floor(max);
+//   return Math.floor(Math.random() * (maxNumber - minNumber)) + minNumber;
+// };
