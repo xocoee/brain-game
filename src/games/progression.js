199c204799e3930191e72c09f11dbb412a59aaf8
xocoee <141821998+xocoee@users.noreply.github.com>
2023-10-03 14:26:22 +0300

    fix mentor edits 6

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,20 +1,20 @@
-import getRandomNumber from '../utils.js';
+import getRn from '../utils.js';
 import run from '../index.js';
 
 const description = 'What number is missing in the progression?';
 
-const progersionLenght = getRandomNumber(5, 15); // change length brain-progression
-const maxIndexToProg = progersionLenght - 1;
-const generateProgression = (numberStartProg, stepToProg) => {
-  const progression = [];
-  for (let i = 0; i < progersionLenght; i += 1) {
-    progression.push(numberStartProg + stepToProg * i);
-  }
-  return progression;
-};
 const generateRound = () => {
-  const progression = generateProgression(getRandomNumber(1, 100), getRandomNumber(2, 8));
-  const indexToHide = getRandomNumber(0, maxIndexToProg);
+  const maxLastIndex = 2;
+  const generateProgression = (numberStartProg, stepToProg, length) => {
+    const progression = [];
+    for (let i = 0; i < length; i += 1) {
+      progression.push(numberStartProg + stepToProg * i);
+    }
+    return progression;
+  };
+
+  const progression = generateProgression(getRn(1, 100), getRn(2, 8), getRn(5, 15));
+  const indexToHide = getRn(0, progression.length - maxLastIndex);
   const answer = String(progression[indexToHide]);
   progression[indexToHide] = '..';
   const question = progression.join(' ');
